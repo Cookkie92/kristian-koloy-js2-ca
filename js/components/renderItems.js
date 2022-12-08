9c297e1aa80dfcc9227140054a22a1f0b78940ce
Cookkie92 <74307133+Cookkie92@users.noreply.github.com>
2022-12-08 20:53:51 +0100

    update

--- js/components/renderItems.js
+++ js/components/renderItems.js
@@ -1,2 +1,10 @@
 //here i will render the items fetched from the API
 //and export the function to index.js
+// export function renderProducts(productsToRender) {
+//   const itemsContainer = document.querySelector(".results");
+//   itemsContainer.innerHTML = "";
+
+//   itemsContainer.innerHTML += `<div class="results">
+//                <h3 class="title">${articles.title}</h3>
+//                </div>`;
+// }
--- js/index.js
+++ js/index.js
@@ -1,13 +1,25 @@
 //main page of my javascript..
 //here i will have the framework of all my functions inside a async fetch function
-import { baseUrl } from "./settings/api.js";
+// import { renderProducts } from "./components/renderItems.js";
+// import { baseUrl } from "./settings/api.js";
+const itemsContainer = document.querySelector(".results");
+const baseUrl = "http://localhost:1337/articles/";
 async function getItems() {
   try {
     const response = await fetch(baseUrl);
     const items = await response.json();
 
     console.log(items);
-
+    for (let i = 0; i < items.length; i++) {
+      itemsContainer.innerHTML += `
+      <div class="results">
+            <h1>${items[i].title}</h1>
+           <p> ${items[i].author}</p>
+            <p>${items[i].summary}</p>
+            <button>
+            </div>
+        `;
+    }
     //functions will go here
   } catch (error) {
     console.log(error);
@@ -15,3 +27,5 @@ async function getItems() {
 }
 
 getItems();
+
+//[0].title
