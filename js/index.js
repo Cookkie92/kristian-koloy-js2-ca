26610d51921b46719dc8e379c4726016a5af531c
Cookkie92 <74307133+Cookkie92@users.noreply.github.com>
2022-12-08 20:34:28 +0100

    update

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 //main page of my javascript..
 //here i will have the framework of all my functions inside a async fetch function
-
+import { baseUrl } from "./settings/api.js";
 async function getItems() {
   try {
-    const response = await fetch(url);
+    const response = await fetch(baseUrl);
     const items = await response.json();
 
     console.log(items);
--- js/settings/api.js
+++ js/settings/api.js
@@ -1,1 +1,1 @@
-export const baseUrl = "http://localhost:1337/api/";
+export const baseUrl = "http://localhost:1337/articles/";
