a4bc281648a39a1d2ffe47c8ebee5658bf843573
Cookkie92 <74307133+Cookkie92@users.noreply.github.com>
2022-12-11 00:27:27 +0100

    update

--- js/index.js
+++ js/index.js
@@ -8,35 +8,24 @@ async function getItems() {
   try {
     const response = await fetch(baseUrl);
     const items = await response.json();
-    // let articlesArray = [];
-    // let getArticles = JSON.parse(localStorage.getItem("articles"));
-
-    // articlesArray = items.map((x) => {
-    //   return {
-    //     author: x.author,
-    //     title: x.title,
-    //     summary: x.summary,
-    //     id: x.id,
-    //     favorite: "false",
-    //   };
-    // });
-
-    // localStorage.setItem("articles", JSON.stringify(articlesArray));
 
     console.log();
 
     items.forEach((articles) => {
       itemsContainer.innerHTML += `
-      <div class="result">
+      <div class="results">
             <h1>${articles.title}</h1>
             <p> ${articles.author}</p>
             <p>${articles.summary}</p>
-            <i class="toggle-x" data-id="${articles.id}" data-name="${articles.title}">\u00D7</i>
+            <i class="fa fa-heart" data-id="${articles.id}" data-title="${articles.title}"></i>
       </div>
       `;
     });
+    const heartButtons = document.querySelectorAll(".results i");
 
-    //functions will go here
+    heartButtons.forEach((button) => {
+      button.addEventListener("click", handleClick);
+    });
   } catch (error) {
     console.log(error);
   }
@@ -45,6 +34,62 @@ async function getItems() {
 getItems();
 
 function handleClick() {
-  this.classList.toggle();
-  this.classList.toggle();
+  event.target.classList.toggle("fa");
+  event.target.classList.toggle("far");
+
+  const id = this.dataset.id;
+  const title = this.dataset.title;
+  const author = this.dataset.author;
+  const summary = this.dataset.summary;
+
+  const savedFavs = getSavedFavorites();
+
+  const articleExist = savedFavs.find(function (fav) {
+    return fav.id === id;
+  });
+
+  console.log("articleExist", articleExist);
+
+  if (articleExist === undefined) {
+    const article = {
+      author: author,
+      title: title,
+      summary: summary,
+      id: id,
+    };
+    savedFavs.push(article);
+    saveFavorites(savedFavs);
+  } else {
+    const newFavs = savedFavs.filter((fav) => fav.id !== id);
+    saveFavorites(newFavs);
+  }
 }
+
+function getSavedFavorites() {
+  const favs = localStorage.getItem("favorites");
+
+  if (favs === null) {
+    return [];
+  } else {
+    return JSON.parse(favs);
+  }
+}
+
+function saveFavorites(favs) {
+  localStorage.setItem("favorites", JSON.stringify(favs));
+}
+
+// let articlesArray = [];
+// let getArticles = JSON.parse(localStorage.getItem("articles"));
+
+// articlesArray = items.map((x) => {
+//   return {
+//     author: x.author,
+//     title: x.title,
+//     summary: x.summary,
+//     id: x.id,
+//     favorite: "false",
+//   };
+// });
+
+// localStorage.setItem("articles", JSON.stringify(articlesArray));
